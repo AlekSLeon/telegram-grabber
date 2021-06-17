0d06b5c1cdacb16354857396612d5df2ab631006
STARTSPr0 <sstartss@yandex.ru>
2021-06-17 13:44:36 +0300

    Update bot_grabber.py
    Работает с пирогом 1.2.х

--- python_bot/bot_grabber.py
+++ python_bot/bot_grabber.py
@@ -37,7 +37,7 @@ def send_post(client, message):
         msg_id = item[1]
 
     send = app.get_messages(username, msg_id)
-    send.forward(bd.get_channel(), as_copy=True)
+    send.forward(bd.get_channel())
 
 
 if __name__ == '__main__':
